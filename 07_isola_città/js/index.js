4f2f367ca11507ba0c98eacf68ee7b985291b46e
irene <36738362+iretrtr@users.noreply.github.com>
2019-04-18 15:41:00 +0200

    license update

--- 07_isola_città/js/index.js
+++ 07_isola_città/js/index.js
@@ -1,3 +1,11 @@
+// Irene Trotta @iretrtr © 2018 MIT License
+// Digital cartography and ethnographic survey of online communities​
+// online-community-maps 7 | island + cities improved | Città Sant' Angelo, IT | 1.2019
+// Educational purpose, master graduation project prototype
+//
+// what if online communities could be real places? how would their map be?
+// prototype > create a map from/with subreddit community data
+
 //var num_utenti = numRandom(10000);
 var num_utenti = 20000;
 var num_topics;
